346452549fffa3c2315a1d0dbbd87fd2d31dcce7
Yurii Vients <yuriivients@Yuriis-MacBook-Pro.local>
2017-09-05 23:56:02 +0300

    add project

---
+++ AnimationContrains/AnimationContrains/ViewController.swift
@@ -0,0 +1,50 @@
+//
+//  ViewController.swift
+//  AnimationContrains
+//
+//  Created by Yurii Vients on 9/1/17.
+//  Copyright © 2017 Yurii Vients. All rights reserved.
+//
+
+import UIKit
+import TextFieldEffects
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var LeftConstraint: NSLayoutConstraint!
+    var animationSuccess = false
+    
+    var selected = false
+    @IBOutlet weak var AnView: UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    func AnimatedView() {
+        
+       // self.view.layoutIfNeeded()
+        
+        UIView.animate(withDuration: 0.5, animations: {
+            self.LeftConstraint.constant = self.animationSuccess ? -20 : -self.AnView.frame.size.width
+        }) { (success) in
+            self.animationSuccess = self.animationSuccess ? false :true
+            self.view.layoutIfNeeded()
+        }
+    }
+   
+    @IBAction func ActionButton(_ sender: UIButton) {
+        AnimatedView()
+        
+        
+    }
+    
+   
+}
+
---
+++ AnimationContrains/Pods/Target Support Files/Pods-AnimationContrains/Pods-AnimationContrains-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_AnimationContrains : NSObject
+@end
+@implementation PodsDummy_Pods_AnimationContrains
+@end
