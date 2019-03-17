d95ef6ba89540986f3d16b401634d01d59efbd9c
Yurii Vients <yuriivients@Yuriis-MacBook-Pro.local>
2019-03-17 21:20:45 +0200

    Revert "commit 2"
    This reverts commit 8a5644529f335fc229bf3677d38e0eb2d4c52313.

--- AnimationContrains/AnimationContrains/ViewController.swift
+++ AnimationContrains/AnimationContrains/ViewController.swift
@@ -19,12 +19,9 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("commit 2")
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    
-    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
